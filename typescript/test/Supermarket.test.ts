4adf2b3207c92f7dbaf84246d9f40edd84b73528
Nick <b10303008@gmail.com>
2020-06-08 02:52:53 +0800

    add FiveForAmount test

--- typescript/test/Supermarket.test.ts
+++ typescript/test/Supermarket.test.ts
@@ -42,4 +42,60 @@ describe('Supermarket', function () {
             this.verifyAsJSON(receipt)
         });
     });
+
+    describe('TenPercentDiscount Discount', function () {
+        const rice: Product = new Product("rice", ProductUnit.Kilo);
+    
+        const catalog: SupermarketCatalog = new FakeCatalog();
+        catalog.addProduct(rice, 2.49);
+    
+        const teller: Teller = new Teller(catalog);
+
+        it('Buy 1 kilo rice, when 10% discount is not active.', function (this: any) {
+            cart.addItemQuantity(rice, 3);
+            const receipt: Receipt = teller.checksOutArticlesFrom(cart);
+            this.verifyAsJSON(receipt)
+        });
+
+        it('Buy 1 kilo rice, when 10% discount is active.', function (this: any) {
+            teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, rice, 10);
+            cart.addItemQuantity(rice, 3);
+            const receipt: Receipt = teller.checksOutArticlesFrom(cart);
+            this.verifyAsJSON(receipt)
+        });
+    });
+
+    describe('FiveForAmount Discount', function () {
+        const toothbrush: Product = new Product("toothbrush", ProductUnit.Each);
+    
+        const catalog: SupermarketCatalog = new FakeCatalog();
+        catalog.addProduct(toothbrush, 1.79);
+    
+        const teller: Teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.FiveForAmount, toothbrush, 7.49);
+
+        it('Buy 4 toothbrushes', function (this: any) {
+            cart.addItemQuantity(toothbrush, 4);
+            const receipt: Receipt = teller.checksOutArticlesFrom(cart);
+            this.verifyAsJSON(receipt)
+        });
+
+        it('Buy 5 toothbrushes', function (this: any) {
+            cart.addItemQuantity(toothbrush, 5);
+            const receipt: Receipt = teller.checksOutArticlesFrom(cart);
+            this.verifyAsJSON(receipt)
+        });
+
+        it('Buy 6 toothbrushes', function (this: any) {
+            cart.addItemQuantity(toothbrush, 6);
+            const receipt: Receipt = teller.checksOutArticlesFrom(cart);
+            this.verifyAsJSON(receipt)
+        });
+
+        it('Buy 13 toothbrushes', function (this: any) {
+            cart.addItemQuantity(toothbrush, 13);
+            const receipt: Receipt = teller.checksOutArticlesFrom(cart);
+            this.verifyAsJSON(receipt)
+        });
+    });
 });
