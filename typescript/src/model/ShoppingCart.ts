56f0e79fdbf6b956c3d58c65cf623782f89e335b
Nick <b10303008@gmail.com>
2020-06-11 02:16:41 +0800

    extract method

--- typescript/src/model/ShoppingCart.ts
+++ typescript/src/model/ShoppingCart.ts
@@ -53,39 +53,42 @@ export class ShoppingCart {
             if (offers[productName]) {
                 const offer : Offer = offers[productName];
                 const unitPrice: number= catalog.getUnitPrice(product);
-                let quantityAsInt = quantity;
-                let discount : Discount|null = null;
-                let x = 1;
-                if (offer.offerType == SpecialOfferType.ThreeForTwo) {
-                    x = 3;
-
-                } else if (offer.offerType == SpecialOfferType.TwoForAmount) {
-                    x = 2;
-                    if (quantityAsInt >= 2) {
-                        const total = offer.argument * Math.floor(quantityAsInt / x) + quantityAsInt % 2 * unitPrice;
-                        const discountN = unitPrice * quantity - total;
-                        discount = new Discount(product, "2 for " + offer.argument, discountN);
-                    }
-
-                } if (offer.offerType == SpecialOfferType.FiveForAmount) {
-                    x = 5;
-                }
-                const numberOfXs = Math.floor(quantityAsInt / x);
-                if (offer.offerType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
-                    const discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
-                    discount = new Discount(product, "3 for 2", discountAmount);
-                }
-                if (offer.offerType == SpecialOfferType.TenPercentDiscount) {
-                    discount = new Discount(product, offer.argument + "% off", quantity * unitPrice * offer.argument / 100.0);
-                }
-                if (offer.offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
-                    const discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
-                    discount = new Discount(product, x + " for " + offer.argument, discountTotal);
-                }
+                let discount : Discount|null = this.calcDiscount(product, offer, unitPrice, quantity)
                 if (discount != null)
                     receipt.addDiscount(discount);
             }
-
         }
+    }
+
+    private calcDiscount(product: Product, offer: Offer, unitPrice: number, quantityAsInt: number): Discount|null {
+        let discount : Discount|null = null;
+        let x = 1;
+        if (offer.offerType == SpecialOfferType.ThreeForTwo) {
+            x = 3;
+
+        } else if (offer.offerType == SpecialOfferType.TwoForAmount) {
+            x = 2;
+            if (quantityAsInt >= 2) {
+                const total = offer.argument * Math.floor(quantityAsInt / x) + quantityAsInt % 2 * unitPrice;
+                const discountN = unitPrice * quantityAsInt - total;
+                discount = new Discount(product, "2 for " + offer.argument, discountN);
+            }
+
+        } if (offer.offerType == SpecialOfferType.FiveForAmount) {
+            x = 5;
+        }
+        const numberOfXs = Math.floor(quantityAsInt / x);
+        if (offer.offerType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
+            const discountAmount = quantityAsInt * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
+            discount = new Discount(product, "3 for 2", discountAmount);
+        }
+        if (offer.offerType == SpecialOfferType.TenPercentDiscount) {
+            discount = new Discount(product, offer.argument + "% off", quantityAsInt * unitPrice * offer.argument / 100.0);
+        }
+        if (offer.offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
+            const discountTotal = unitPrice * quantityAsInt - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+            discount = new Discount(product, x + " for " + offer.argument, discountTotal);
+        }
+        return discount;
     }
 }
